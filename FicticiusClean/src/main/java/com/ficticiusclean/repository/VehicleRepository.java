58b0f67ec8ffff316e8485d59d8427725d55d9a6
Eduardo Mira <53952033+eduardo-mira@users.noreply.github.com>
2023-11-27 16:58:24 -0300

    Limpeza

--- FicticiusClean/src/main/java/com/ficticiusclean/repository/VehicleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ficticiusclean.repository;
-
-import com.ficticiusclean.entity.VehicleInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VehicleRepository extends JpaRepository<VehicleInfo, Long> {
-
-}
